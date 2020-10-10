2445b62f4f5ba6955aca25804149fd5f7e33ac57
Your Name <ashish.gcp.verma@gmail.com>
2020-10-10 12:20:52 -0700

    SpringBoot Kafka Example

---
+++ src/test/java/com/ashish/kafka/springKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class springKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
